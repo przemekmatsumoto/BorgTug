d5dfeea554f940105e053a8f0f8e7f2f8514818a
Przemysław Pieczara <przemekpieczara@op.pl>
2025-08-02 01:07:14 +0200

    Server setup and delete unnecessary files/lines

--- Linux (debian) server structure/home/remote-backup/backup/run_backup.sh
+++ Linux (debian) server structure/home/remote-backup/backup/run_backup.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
 CONFIG_FILE="/home/remote-backup/backup/backup.conf"
 CLIENT="$1"
-ARCHIVE="$2"
-DESTDIR="${3:-/}"
+TIMESTAMP=$(date '+%Y-%m-%d_%H:%M')
 
 function notify_slack {
   local msg="$1"
@@ -15,34 +13,30 @@ function notify_slack {
     curl -s -X POST --data "{\"text\": \"$msg\"}" "$webhook" >/dev/null || true
 }
 
+
 SSH_HOST=$(crudini --get "$CONFIG_FILE" "$CLIENT" ssh_host)
-SOCKET_LOCAL=$(crudini --get "$CONFIG_FILE" "$CLIENT" socket_local)
 SOCKET_REMOTE=$(crudini --get "$CONFIG_FILE" "$CLIENT" socket_remote)
+SOCKET_LOCAL=$(crudini --get "$CONFIG_FILE" "$CLIENT" socket_local)
 BORG=$(crudini --get "$CONFIG_FILE" "$CLIENT" borg_binary)
 SOCAT=$(crudini --get "$CONFIG_FILE" "$CLIENT" socat_binary)
 PASSCMD=$(crudini --get "$CONFIG_FILE" "$CLIENT" passcommand)
 REPO=$(crudini --get "$CONFIG_FILE" "$CLIENT" repo)
-
-REMOTE_CMD=$(cat <<EOF
-mkdir -p "$DESTDIR" && \
-cd "$DESTDIR" && \
-sudo -E BORG_PASSCOMMAND='$PASSCMD' \
-$BORG --rsh="sh -c 'exec $SOCAT STDIO UNIX-CONNECT:$SOCKET_LOCAL'" \
-extract --progress ${REPO}::${ARCHIVE}
-EOF
-)
-
+DIRS=$(crudini --get "$CONFIG_FILE" "$CLIENT" dirs)
 
 SSH_CMD=(
-  ssh -o BatchMode=yes -R "${SOCKET_LOCAL}:${SOCKET_REMOTE}" "$SSH_HOST"
-  "$REMOTE_CMD"
+  ssh -o BatchMode=yes -R "${SOCKET_REMOTE}:${SOCKET_LOCAL}" -t "$SSH_HOST"
+  "sudo -E BORG_PASSCOMMAND='$PASSCMD' \
+    $BORG --rsh=\"sh -c 'exec $SOCAT STDIO UNIX-CONNECT:$SOCKET_REMOTE'\" \
+    create --progress ${REPO}::backup-${TIMESTAMP} $DIRS"
 )
 
 if ! "${SSH_CMD[@]}"; then
-  notify_slack "❌ Restore *${CLIENT}* :: *${ARCHIVE}* failed to ${DESTDIR}"
+  MSG="❌ Backup *${CLIENT}* failed at ${TIMESTAMP}. Serwer albo klient spadł z rowerka, trzeba ratować!"
+  notify_slack "$MSG"
   exit 1
 else
-  notify_slack "✅ Restore *${CLIENT}* :: *${ARCHIVE}* completed to ${DESTDIR}"
+  MSG="✅ Backup *${CLIENT}* succeeded at ${TIMESTAMP}"
+  notify_slack "$MSG"
 fi
 
 exit 0
